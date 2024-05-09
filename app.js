e3f4acd6cbf15b7a18da905aa61f37c870991917
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2024-05-08 23:27:56 -0300

    Refactoring

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var http = require('http');
+const express = require('express');
+const app = express();
 
-var server = http.createServer((req, res) => {
-  var url = req.url;
-
-  if(url == '/tecnologia') {
-    res.end("<html><body><h1>Noticias de tecnologia</h1></body></html>");
-  }
-  else if (url == '/moda') {
-    res.end("<html><body><h1>Noticias de moda</h1></body></html>");
-  }
-  else if (url == '/beleza') {
-    res.end("<html><body><h1>Noticias de beleza</h1></body></html>");
-  }
-  else {
-    res.end("<html><body><h1>Portal de noticias</h1></body></html>");
-  }
-
-  
+app.get('/', (req, res) => {
+  res.send("<html><body><h1>Portal de noticias</h1></body></html>")
 });
 
-server.listen(3000);
+app.get('/tecnologia', (req, res) => {
+  res.send("<html><body><h1>Noticias de tecnologia</h1></body></html>");
+});
+
+app.get('/moda', (req, res) => {
+  res.send("<html><body><h1>Noticias de moda</h1></body></html>");
+});
+
+app.get('/beleza', (req, res) => {
+  res.send("<html><body><h1>Noticias de beleza</h1></body></html>");
+});
+
+app.listen(3000, () => {
+  console.log('Server Up');
+});
