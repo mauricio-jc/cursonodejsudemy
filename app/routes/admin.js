9dc75a6c56aa8ac35c9467d4c9a619890c186eda
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2024-05-24 19:01:14 -0300

    Corrigido rotas.

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,11 @@
 const noticeAddRequestValidator = require('../requests_validators/notice_add_request_validator');
 
 module.exports = (app) => {
-  app.get('/notice/add', (req, res) => {
+  app.get('/notice_add', (req, res) => {
     app.app.controllers.admin_controller.notice_add(app, req, res);
   });
 
-  app.post('/notice/save', noticeAddRequestValidator, (req, res) => {
+  app.post('/notice_save', noticeAddRequestValidator, (req, res) => {
     app.app.controllers.admin_controller.notice_save(app, req, res);
   });
 }
